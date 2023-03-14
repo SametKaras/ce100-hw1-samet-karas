2f93b0ed97385ab6e94ea71215f3d83f9c712007
SametKaras <sametkaras.tr@gmail.com>
2023-03-14 11:52:50 +0300

    RecursiveMergeSort and Unit Test

--- ce100-hw1-sln/ce100-hw1-algo-lib-cs/Class1.cs
+++ ce100-hw1-sln/ce100-hw1-algo-lib-cs/Class1.cs
@@ -35,4 +35,76 @@ namespace ce100_hw1_algo_lib_cs
             return arr;
         }
     }
+    public class RecursiveMergeSort
+    {
+        /// <summary>
+        /// It divides the array it receives as input into binary groups 
+        /// until it becomes the smallest and sorts the array using the comparison method.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static int[] MergeSortRecursive(ref int[] data, int left, int right)
+        {
+            if (left < right)
+            {
+                int m = left + (right - left) / 2;
+
+                MergeSortRecursive(ref data, left, m);
+                MergeSortRecursive(ref data, m + 1, right);
+                Merge(ref data, left, m, right);
+            }
+            return data;
+        }
+
+        private static void Merge(ref int[] data, int left, int mid, int right)
+        {
+            int i, j, k;
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (i = 0; i < n1; i++)
+                L[i] = data[left + i];
+
+            for (j = 0; j < n2; j++)
+                R[j] = data[mid + 1 + j];
+
+            i = 0;
+            j = 0;
+            k = left;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    data[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    data[k] = R[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                data[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                data[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
 }
--- ce100-hw1-sln/ce100-hw1-algo-test-cs/UnitTest1.cs
+++ ce100-hw1-sln/ce100-hw1-algo-test-cs/UnitTest1.cs
@@ -2,6 +2,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
+namespace ce100_hw1_algo_lib_cs.Tests
+{
+
+}
+
 namespace ce100_hw1_algo_test_cs
 {
     [TestClass]
@@ -21,6 +26,26 @@ namespace ce100_hw1_algo_test_cs
             int size = series.Length;
 
             int[] ff = SelectionSort.SelectionSortFunction(series);
+
+            int[] sorted = (int[])series.Clone();
+            Array.Sort(sorted);
+
+            CollectionAssert.AreEqual(sorted, ff);
+        }
+        [TestMethod()]
+        public void MergeSortRecursiveTest()
+        {
+            int[] series = new int[10000];
+            Random random = new Random();
+
+            for (int i = 0; i < series.Length; i++)
+            {
+                series[i] = random.Next(0, 10000);
+            }
+
+            int size = series.Length;
+
+            int[] ff = RecursiveMergeSort.MergeSortRecursive(ref series, 0, size - 1);
 
             int[] sorted = (int[])series.Clone();
             Array.Sort(sorted);
